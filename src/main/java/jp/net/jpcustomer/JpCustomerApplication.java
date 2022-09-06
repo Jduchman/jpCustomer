0846c2560bb14b7d478387657583b6a1545a3adc
Jduchman <jorge.duchman.27@unsch.edu.pe>
2022-09-06 16:47:21 -0500

    first commit

---
+++ src/main/java/jp/net/jpcustomer/JpCustomerApplication.java
@@ -0,0 +1,13 @@
+package jp.net.jpcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/net/jpcustomer/JpCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.net.jpcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
